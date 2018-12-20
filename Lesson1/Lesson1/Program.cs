d5aec28e5cdb416ef45c895659c5fbf0481b3063
MarikGar <marikgar@mail.ru>
2018-12-20 15:18:02 +0300

    no errors

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -2,33 +2,36 @@
 
 namespace Sravnenie
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("введите строку");
-            string text = Console.ReadLine();
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Console.WriteLine( "введите строку" );
+			string text = Console.ReadLine();
 
-            Console.WriteLine("нечетных символов {0} ", s1.Length);
-            Console.WriteLine("четных символов {0} ", s0.Length);
-            Console.ReadKey();
-        }
-        private (string s1, string s0) Obrabotka(string text)
-        {
-            string s0 = null;
-            string s1 = null;
-            for (int i = 0; i < text.Length; i++)
-            {
-                if ((i % 2 == 0))
-                {
-                    s0 = s0 + text[i];
-                }
-                else
-                {
-                    s1 = s1 + text[i];
-                }
-            }
-            return s0,s1;
-        }
-    }
+			var res = Obrabotka( text );
+
+			Console.WriteLine( "нечетных символов {0} ", res.s0 );
+			Console.WriteLine( "четных символов {0} ", res.s1 );
+			Console.ReadKey();
+		}
+
+		static (string s1, string s0) Obrabotka( string text )
+		{
+			string s0 = null;
+			string s1 = null;
+			for (int i = 0; i < text.Length; i++)
+			{
+				if ((i % 2 == 0))
+				{
+					s0 = s0 + text[ i ];
+				}
+				else
+				{
+					s1 = s1 + text[ i ];
+				}
+			}
+			return (s0, s1);
+		}
+	}
 }
