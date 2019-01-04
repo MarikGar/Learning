8653db5add88fb43855435a8e10e1d7d66e1a369
MarikGar <marikgar@mail.ru>
2019-01-04 16:59:55 +0300

    приукрасил код

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -1,68 +1,70 @@
 ﻿using System;
+using Learning;
 
 namespace Snake
 {
 	class Program
 	{
-		public static void Main()
+		const char Wall = '■';
+
+		// прочитай про параметры программы в 3ей главе
+		// не надо спрашивать у юзера больше ничо
+		public static void Main( string[] args ) 
+		{
+			var Width = int.Parse( args != null && args.Length > 1 ? args[ 0 ] : "40" );
+			var Height = int.Parse( args != null && args.Length > 2 ? args[ 1 ] : "20" );
+
+			Game( Width, Height );
+
+			// поскольку юзер все равно проиграет, то выведем
+			Console.SetCursorPosition( 0, Height + 1 );
+			Utils.Println( "Вы проиграли", ConsoleColor.Red );
+			Console.ReadKey();
+		}
+
+		static void Game( int width, int height )
 		{
 			Console.Clear();
-			Console.SetCursorPosition( 0, 0 );
-			int posx = Console.CursorTop;      // курсор в точке с координатой х=0
-			int posy = Console.CursorLeft;     // курсор в точке с координатой у=0
-			Console.WriteLine( "Введите длину сторон поля:  " );
-			int len = int.Parse( Console.ReadLine() );
-			Console.Clear();
-			DrawHorizontal( posx, posy, len );		
-			DrawVertical( posx, posy, len );
-			SnakeBody( posx, posy );
-			Console.WriteLine();
-			Console.ReadKey();
-
-			// вот теперь делай что хочешь, я пока посмотрю
-			//читай еще раз 4 главу сначала  внимательно: как задаются методы, и как они вызываются
+			DrawBox( width, height );
 		}
 
 
-		// 3) какого хрена здесь нет пустых строк? эта другая функция, их надо разделять визуально
+		#region Box drawing									 
+		static void DrawBox( int width, int height )
+		{
+			Console.ForegroundColor = ConsoleColor.Cyan;
+
+			DrawHorizontal( 0, 0, width ); 
+			DrawHorizontal( 0, height, width );
+
+			DrawVertical( 0, 0, height );
+			DrawVertical( width, 0, height );	// думаю, почему не нарисовался последний квадратик
+		}
+
+
 		static void DrawHorizontal( int x, int y, int len )    // начало в x,y длиной len
 		{
-			for (int i = 0; i < len; ++i)				
-			{
-				Console.SetCursorPosition( i, y );
-				Console.Write( "s" );
-			}
-			for (int p = 0; p < len; ++p)
-			{
-				Console.SetCursorPosition( p, (y + len) );
-				Console.Write( "s" );
-			}	  	  			
+			var line = new string( Wall, len );
+			Console.SetCursorPosition( x, y );
+			Console.Write( line );
 		}
 
-
-		// то же самое
 		static void DrawVertical( int x, int y, int len )  // принимает 3 параметра
 		{
 			for (int i = 0; i < len; ++i)
 			{
 				Console.SetCursorPosition( x, i );
-				Console.Write( "s" );
-			}
-			for (int k = 0; k <= len; ++k)
-			{
-				Console.SetCursorPosition( (x + len), k );
-				Console.Write( "s" );
+				Console.Write( Wall );
 			}
 		}
+		#endregion
 
-
-
-		static void SnakeBody(int x , int y)	// функция тела змеи
+		#region Snake drawing
+		static void SnakeBody( int x, int y )   // функция тела змеи   // это мнен для ориентира
 		{
-			Console.SetCursorPosition( (x+1), (y+1) );
+			Console.SetCursorPosition( (x + 3), (y + 3) );
 			Console.Write( "*" );
 		}
-																								
-
+		#endregion
 	}
 }
