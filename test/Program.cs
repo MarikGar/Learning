7d648d78c45ad0dd2266a2beb67c85715ea4aa46
MarikGar <marikgar@mail.ru>
2019-01-04 22:52:26 +0300

    тест

---
+++ test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+class Sample
+{
+	public static void Main()
+	{
+		ConsoleKeyInfo but;
+
+		do
+		{
+			Console.WriteLine( "Нажмите кнопку Escape, что бы выйти " );
+
+			
+
+			while (Console.KeyAvailable == false)
+				Thread.Sleep( 250 ); // Loop until input is entered.
+
+			but = Console.ReadKey( true );
+			Console.WriteLine( "Вы нажали  '{0}'", but.Key );
+		} while (but.Key != ConsoleKey.Escape);
+	}
+}
